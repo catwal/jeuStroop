ef8d6665ad150bfbf9e5ac28a6437f7b4da684d8
catwal <kitekate.cw@gmail.com>
2019-04-09 21:18:38 +0200

    affichage items, score et timerUser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,66 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import * as moment from "moment";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  public order1 = [
+    { id: "0", item: "jaune", color: "yellow", response: true },
+    { id: "1", item: "vert", color: "blue", response: false },
+    { id: "2", item: "bleu", color: "red", response: false },
+    { id: "3", item: "noir", color: "black", response: true },
+    { id: "4", item: "rose", color: "red", response: false }
+  ];
+
+  public count: number = 0;
+  public items: any;
+  public level: number = 1;
+  public item: any;
+  public score: number = 0;
+  public startTime: string;
+  public currentTime: string;
+  public diffTime: number;
+  public recordTime: Array<any> = new Array<any>();
 
   constructor(public navCtrl: NavController) {
-
+    this.itemRandomly();
+    this.resetTime();
   }
 
+  resetTime() {
+    this.startTime = moment().format("ss.SSS");
+    console.log("resetTime " + this.startTime);
+  }
+
+  itemRandomly() {
+    this.item = this.order1[Math.floor(Math.random() * this.order1.length)];
+    console.log(this.item);
+    this.items = this.item;
+  }
+
+  onAnswer(event, response) {
+    if (event) {
+      this.count += 1;
+      this.currentTime = moment().format("ss.SSS");
+      this.diffTime = +this.currentTime - +this.startTime;
+      console.log(this.diffTime);
+      this.recordTime.push(this.diffTime);
+      console.log(this.recordTime);
+      this.resetTime();
+    }
+
+    if (response === this.item.response) {
+      this.score += 1;
+    }
+    /* faire un enregistrement des temps et des erreurs
+    faire un model avec 0 raté et 1 reussie et les temps associés */
+    this.itemRandomly();
+  }
+
+  nextTest() {
+    /* faire passer à un autre niveau si un score de 10 atteint */
+  }
 }
